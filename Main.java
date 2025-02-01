fc02262392b6e805d7d65ba618764475b522d1b4
ChristineGoGo <brandywang22@gmail.com>
2025-02-01 04:58:42 -0800

    new tests

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-        int[][] input = {
-            {0, 1, 2},
-            {2, 3, 4},
-            {5, 6, 7}
-        };
-        for (int j = 0; j < input.length; j++) {
-            for (int i = 0; i < input[j].length; i++) {
-                System.out.println(input[j][i]);
-            }
+        String str = "enchanted";
+        String substring = "ant";
+        String[] parts = str.split("ant");
+
+        for (int i = 0; i < parts.length; i++) {
+            System.out.println(parts[i]);
         }
+        System.out.println(str.substring(1));
     }
 
 
